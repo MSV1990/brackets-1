6ba7415cb01cf024fa69319979710000c578ba01
msv1990 <htconemailbox@gmail.com>
2019-09-22 23:55:02 +0300

    fetch: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = str.split('');
+  if(brackets.length % 2 !== 0){return false};
+  let openBrackets = [];
+  let closeBrackets = [];
+  for( let i =0; i < bracketsConfig.length; i++){
+   openBrackets.push(bracketsConfig[i][0]);
+   closeBrackets.push(bracketsConfig[i][1]);
+
+  }
+  
+let stack = [];
+
+  for(let i=0; i < brackets.length; i++){
+    if(openBrackets.includes(brackets[i])){
+      if (closeBrackets.indexOf(brackets[i]) == stack[stack.length - 1]) {
+        stack.pop()
+      }
+    else stack.push(openBrackets.indexOf(brackets[i]));
+      }
+   
+   else if(closeBrackets.includes(brackets[i])){
+    if(stack[stack.length - 1] === closeBrackets.indexOf(brackets[i])){
+      stack.pop();
+    
+    }
+      
+      else { 
+        return false;
+      }
+    
+
+
+    }
+   
+  } 
+  return stack.length === 0;
+  
+
 }
+
